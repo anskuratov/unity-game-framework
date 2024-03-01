18d1f9536b5c87a61750e61b1c8f6cfaf7067f54
Andrei Skuratov <skuratovandry@gmail.com>
2024-03-01 13:26:43 +0300

    Добавил PathUtils

---
+++ Assets/Scripts/Utils/PathUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace AS.Framework.Utils
+{
+    public static class PathUtils
+    {
+        public static string GetStreamingAssetsPath(string fileNameWithExtension)
+        {
+            return Path.Combine(Application.streamingAssetsPath, fileNameWithExtension);
+        }
+
+        public static string GetStreamingAssetsPath(string fileName, string extension)
+        {
+            return Path.Combine(Application.streamingAssetsPath, string.Format("{0}.{1}", fileName, extension)); 
+        }
+    }
+}
